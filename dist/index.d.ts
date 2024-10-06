427b13c9f48117bd0a4fa144b30413bbd7fdd040
Venkat <gurrala.gvnreddy@gmail.com>
2024-10-05 22:30:03 -0400

    Update the required files for npm

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+import * as react_jsx_runtime from 'react/jsx-runtime';
+
+declare const useVReactMultiSelect: () => {
+    VReactMultiSelect: ({ headerLeft, headerRight, inputData, selectedInoutData, returnValues, }: Props) => react_jsx_runtime.JSX.Element;
+};
+
+export { useVReactMultiSelect as default };
