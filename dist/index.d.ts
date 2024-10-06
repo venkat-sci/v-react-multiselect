d805ed62784bc9b853e31d4b10e4026f50abb4a3
Venkat <gurrala.gvnreddy@gmail.com>
2024-10-06 18:24:07 -0400

    Deleted dist folder to rearrage the new foler stursture

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-
-declare const useVReactMultiSelect: () => {
-    VReactMultiSelect: ({ headerLeft, headerRight, inputData, selectedInoutData, returnValues, }: Props) => react_jsx_runtime.JSX.Element;
-};
-
-export { useVReactMultiSelect as default };
