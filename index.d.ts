3b6e931c87db37076e28fbb0a45e51f15536acfa
Venkat <gurrala.gvnreddy@gmail.com>
2024-10-07 17:17:41 -0400

    npm package build version 9

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
 import * as react_jsx_runtime from 'react/jsx-runtime';
 
-declare const VReactMultiSelect: ({ headerLeft, headerRight, inputData, selectedInputData, returnValues, }: Props) => react_jsx_runtime.JSX.Element;
+declare const VReactMultiSelect: ({ className, headerLeft, headerRight, inputData, selectedInputData, returnValues, }: Props) => react_jsx_runtime.JSX.Element;
 
 export { VReactMultiSelect as default };
